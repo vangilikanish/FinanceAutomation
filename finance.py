8bfbdecf90ce6ba421cee17d74ef785c6b8afe9a
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2020-08-17 22:20:04 -0400

    Update finance.py

--- finance.py
+++ finance.py
@@ -1,11 +1,1 @@
-'''
-import csv
 
-row_5 = 0
-
-with open('/Users/kanish/Desktop/Finance(Delete)/exampleData.csv') as csvf:
-    csv_reader = csv.reader(csvf)
-    for row in csvf:
-        if row[3] == 'Debit':
-            print('Hello')
-'''
