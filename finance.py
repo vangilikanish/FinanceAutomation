ae2caa562e711d601f116aa9d7e2bed7499c9d41
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2020-08-18 13:06:57 -0400

    CSV done
    Found out how to work with csv files

--- finance.py
+++ finance.py
@@ -1,6 +1,17 @@
 import csv
 
-with open('example.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
-        print(', '.join(row))
+csvf = 'exampleData.csv'
+rows = []
+
+with open(csvf) as csv_file:  
+    csv_reader = [row for row in csv.reader(csv_file)]
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            rows.append(0)
+        else:
+            debit = row[5]
+            rows.append(debit)
+        line_count += 1
+
+print(rows)  
