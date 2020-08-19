a2a08b639caeb62a65bc1022d6cff9344112e443
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2020-08-19 00:08:04 -0400

    Done Basic CSV
    Did the code to add all my spendings

--- finance.py
+++ finance.py
@@ -1,17 +1,31 @@
 import csv
+import json
 
 csvf = 'exampleData.csv'
-rows = []
+total_spending = []
 
-with open(csvf) as csv_file:  
-    csv_reader = [row for row in csv.reader(csv_file)]
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            rows.append(0)
-        else:
-            debit = row[5]
-            rows.append(debit)
-        line_count += 1
+class expense():
+    with open(csvf) as csv_file:  
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                total_spending.append('0')
+            else:
+                debit = row[5]
+                if debit == '':
+                    total_spending.append('0')
+                else:
+                    total_spending.append(debit)
+            line_count += 1
 
-print(rows)  
+total_spending = list(map(float, total_spending))
+sum = sum(total_spending)
+print(sum)
+
+'''
+csv_reader = [row for row in csv.reader(csv_file)]
+THIS IS THE READER YOU SHOULD USE IF YOU WANT TO TAKE A SINGLE VALUE FORM A CELL
+FOR EX. 'PRINT(CSV_READER[1][0])'
+'''
+
