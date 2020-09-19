f4f4afbccc9589bad62b85d990d6fecb0f4e1a29
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2020-09-18 23:31:38 -0400

    almost done

--- finance.py
+++ finance.py
@@ -1,32 +1,66 @@
 import csv
-import json
 
-csvf = 'exampleData.csv'
-total_spending = []
+class Calc:
+    def __init__(self, file):
+        self.file = file
 
-class expense():
-    with open(csvf) as csv_file:  
-        csv_reader = csv.reader(csv_file, delimiter = ',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                total_spending.append(0.0)
-            else:
-                debit = row[5]
-                if debit == '':
-                    total_spending.append(0.0)
-                else:
-                    total_spending.append(float(debit))
+    def cDate(self):
+        with open(self.file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter = ',')
+            x = 0
+            line_count = 0
+            for row in csv_reader:
+                x += 1
+                if line_count == x:
+                    print(row[0])
             line_count += 1
-    expenses = sum(total_spending)
 
-'''
-csv_reader = [row for row in csv.reader(csv_file)]
-THIS IS THE READER YOU SHOULD USE IF YOU WANT TO TAKE A SINGLE VALUE FORM A CELL
-FOR EX. 'PRINT(CSV_READER[1][0])'
-'''
+    def date(self):
+        with open('exampledata.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter = ',')
+            date = list(csv_reader)
+            fDate = date[1][0]
+            print('Starting from ' + ' to ' + fDate)
 
-'''
-total_spending = list(map(float, total_spending))
-USE THIS TO CONVERT THE VALUES IN A LIST INTO A STRING, INT, ETC
-'''
+    def exp(self):
+        total_expense = []
+        with open(self.file) as csv_file:  
+            csv_reader = csv.reader(csv_file, delimiter = ',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    total_expense.append(0.0)
+                else:
+                    debit = row[5]
+                    if debit == '':
+                        total_expense.append(0.0)
+                    else:
+                        total_expense.append(float(debit))
+                line_count += 1
+        print('Total Expenses: ' + str(sum(total_expense)))
+
+    def inc(self):
+        total_income = []
+        with open(self.file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter = ',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    total_income.append(0.0)
+                else:
+                    credit = row[6]
+                    if credit == '':
+                        total_income.append(0.0)
+                    else:
+                        total_income.append(float(credit))
+                line_count += 1
+        print('Total Income: ' + str(sum(total_income)))
+
+uname = input('Name: ')
+ufile = input('File: ')
+c1 = Calc(ufile)
+print('Hello, ' + uname)
+c1.cDate()
+c1.date()
+c1.exp()
+c1.inc()
